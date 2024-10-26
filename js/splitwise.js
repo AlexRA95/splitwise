2d9290562be817c18307baba11452bff8b527194
AlexRA95 <alexrodral95@gmail.com>
2024-10-26 12:45:27 +0200

    26/10/2024

--- js/splitwise.js
+++ js/splitwise.js
@@ -17,3 +17,37 @@ class Gasto {
     }
     // Completar con los métodos necesarios
 }
+
+
+
+//Crear parte de formulario
+function crearForm() {
+    let cont = document.getElementById("formulario");//Zona donde se coloca el formulario
+    let form = document.createElement("form");//Elemento form
+    form.classList.add("mb-10");//Añadimos clases al form
+    let labelUser= document.createElement("label");//Label para elegir el usuario
+    labelUser.setAttribute("for","user");
+    labelUser.textContent="Elige un usuario:";
+
+    let selectUser = document.createElement("select");//Elemento select de los usuarios
+    selectUser.setAttribute("name","user");
+    selectUser.setAttribute("id","usuario");
+
+    let optionDefault= document.createElement("option");
+    let optionJuan= document.createElement("option");
+    let optionPaco= document.createElement("option");
+    let optionPepe= document.createElement("option");
+    /*
+    <label for="cars">Choose a car:</label>
+    <select name="cars" id="cars" form="carform">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+    </select>
+    */
+    form.appendChild(labelUser,selectUser);
+    cont.appendChild(form);
+}
+
+crearForm();
